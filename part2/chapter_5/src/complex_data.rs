78a17b2764e89e3cbc8cfdb6c94719f758da2ace
homura <preasper0+github@gmail.com>
2023-04-06 20:37:01 +0900

    add

--- part2/chapter_5/src/complex_data.rs
+++ part2/chapter_5/src/complex_data.rs
@@ -3,7 +3,25 @@ use std::error::Error;
 type Undefined = dyn Error;
 
 type CustomerInfo = Undefined;
+type SippingAddress = Undefined;
+type BillingAddress = Undefined;
+type OrderLine = Undefined;
+type BillingAmount = Undefined;
 
 struct Order {
     customer_info: CustomerInfo,
+    sipping_address: SippingAddress,
+    billing_address: BillingAddress,
+    order_lines: Vec<OrderLine>,
+    billing_amount: BillingAmount,
+}
+
+enum ProductCode {
+    WidgetCode(i32),
+    GizmoCode(i32),
+}
+
+enum OrderQuantity {
+    UnitQuantity(i32),
+    Kilogram(i32),
 }
